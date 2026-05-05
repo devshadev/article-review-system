4c66b07f3d5ca350daa6c74542dc76c8471d2ffc
devshadev <umair22432syed@gmail.com>
2026-05-05 20:42:20 +0300

    delete the backend temporarly

--- backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDatabase(mongoUri) {
-  await mongoose.connect(mongoUri, {
-    serverSelectionTimeoutMS: 4000,
-  });
-}
-
-module.exports = { connectDatabase };
--- backend/src/routes/health.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const healthRouter = express.Router();
-
-healthRouter.get("/", (_req, res) => {
-  res.json({ status: "ok" });
-});
-
-module.exports = { healthRouter };
